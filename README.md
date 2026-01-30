bf18308a42caa3c7c604a1c0e97adb9f5d805e79
CoderCatGG <CoderCatGG@gmail.com>
2026-01-30 18:42:14 +0100

    README and improved CLI usage

---
+++ README.md
@@ -0,0 +1,14 @@
+ORBIT (Orbital Runtime Badly Interpreted Thingy)
+---
+
+**DEVELOPMENT WARNING**
+Like any "in development" project, this might go on an unannounced indefinite hiatus at any moment.
+Please read everything below with a grain of salt, as most of these features haven't been implemented.
+
+---
+
+ORBIT is a procedural programming language, aimed towards replacing [`kerboscript`](https://github.com/KSP-KOS/KOS).
+However, ORBIT is also interpretable without the kOS mod. This makes it easier to develop and more adaptable to new developments, such as replacing `ksm` with something else.
+
+## Why ORBIT?
+ORBIT uses a friendlier C-Style syntax, immutability by default, strict typing, and a few functional features. Unlike `kerboscript`, it features modules, bitwise operations, algebraic data types and library support.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, process::exit};
 
 use clap::Parser;
 
 mod lexer;
 
+/// ORBIT Compiler and Interpreter
 #[derive(Debug, Clone, Parser)]
 struct Args {
     #[arg()]
     /// The input file to compile or interpret
     input: PathBuf,
+
+    #[arg(short, long)]
+    /// Set output file. If not specified, will output to STDOUT
+    output: Option<PathBuf>,
+
+    #[arg(short, long)]
+    /// Turn on pretty print when printing to STDOUT/STDERR.
+    /// Only active when `--output` is undefined.
+    pretty_print: bool,
+
+    #[arg(long)]
+    /// Output tokens instead of interpreting
+    emit_tokens: bool,
 }
 
 fn main() {
@@ -16,5 +30,19 @@ fn main() {
 
     let input_text = fs::read_to_string(args.input).expect("Could not read input file");
 
-    eprintln!("{:?}", lexer::to_tokens(&input_text));
+    let tokens = lexer::to_tokens(&input_text);
+
+    if args.emit_tokens {
+        if let Some(out) = args.output {
+            fs::write(out, format!("{:?}", tokens)).expect("Could not write to output path")
+        } else if args.pretty_print {
+            println!("{:#?}", tokens);
+        } else {
+            println!("{:?}", tokens);
+        }
+
+        exit(0)
+    }
+
+    
 }
